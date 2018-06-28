d55ed35a2799696084802d8c9844fa521c908296
Attila Večerek <avecerek@zendesk.com>
2018-06-28 17:49:40 +0200

    Changes parameter name to be less confusing

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,18 +1,18 @@
 const yaml = require('js-yaml');
 const { npmClient } = require('../client');
 
-const readReleaseConfig = path => yaml.safeLoad(path);
+const readReleaseConfig = str => yaml.safeLoad(str);
 
 const buildReleaseConfig = () => {
   const client = npmClient();
 
   return {
+    rollback: true,
     prepare: [
       'git diff-index --quiet HEAD --',
       'git checkout master',
       'git pull --rebase',
       'check-node-version --node $(cat .nvmrc)',
-      'ROLLBACK_COMMIT_ID=$(git rev-parse HEAD)',
       `${client} install`
     ],
     test: [`${client === 'yarn' ? 'yarn' : 'npm run'} travis`],
@@ -22,17 +22,12 @@ const buildReleaseConfig = () => {
       'git commit -m "Update build file"'
     ],
     after_publish: ['git push --follow-tags origin master:master'],
-    after_failure: [
-      '[[ ! -z "$ROLLBACK_COMMIT_ID" ]] && git reset --hard $ROLLBACK_COMMIT_ID',
-      'unset ROLLBACK_COMMIT_ID'
-    ],
     changelog: [
       'offline-github-changelog > CHANGELOG.md',
       'git add CHANGELOG.md',
       'git commit --allow-empty -m "Update changelog"',
       'git push origin master:master'
-    ],
-    after_success: ['unset ROLLBACK_COMMIT_ID']
+    ]
   };
 };
 
