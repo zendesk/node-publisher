8779d3de967c089c52eb162093163d55b59d7a58
Attila Večerek <avecerek@zendesk.com>
2018-12-19 13:48:14 +0100

    Add --preid option for release

--- src/client/npm.spec.js
+++ src/client/npm.spec.js
@@ -1,14 +1,33 @@
 const { publish } = require('./npm');
 
 describe('publish', () => {
-  const options = {
-    nextVersion: 'patch',
-    execCommand: jest.fn()
-  };
+  describe('without preid', () => {
+    const options = {
+      nextVersion: 'patch',
+      preid: undefined,
+      execCommand: jest.fn()
+    };
 
-  it('publishes new version', () => {
-    expect(() => publish(options)).not.toThrow();
-    expect(options.execCommand.mock.calls[0][0]).toBe('npm version patch');
-    expect(options.execCommand.mock.calls[1][0]).toBe('npm publish');
+    it('publishes new version without a prerelease tag', () => {
+      expect(() => publish(options)).not.toThrow();
+      expect(options.execCommand.mock.calls[0][0]).toBe('npm version patch');
+      expect(options.execCommand.mock.calls[1][0]).toBe('npm publish');
+    });
+  });
+
+  describe('with preid', () => {
+    const options = {
+      nextVersion: 'major',
+      preid: 'alpha',
+      execCommand: jest.fn()
+    };
+
+    it('publishes new version with a prerelease tag', () => {
+      expect(() => publish(options)).not.toThrow();
+      expect(options.execCommand.mock.calls[0][0]).toBe(
+        'npm version major --preid=alpha'
+      );
+      expect(options.execCommand.mock.calls[1][0]).toBe('npm publish');
+    });
   });
 });
