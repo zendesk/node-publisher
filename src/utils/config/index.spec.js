0c9b02640e73e63736d462b5e22bbf37d85e5f64
Attila Večerek <avecerek@zendesk.com>
2019-01-20 16:23:38 +0100

    Implement --branch param to support custom branch configuration

--- src/utils/config/index.spec.js
+++ src/utils/config/index.spec.js
@@ -1,11 +1,12 @@
 const { buildReleaseConfig } = require('./');
+const baseEnv = { testRunner: 'travis', branch: 'master' };
 
 describe('buildReleaseConfig', () => {
   describe('when build script is defined in package.json', () => {
-    const baseEnv = { testRunner: 'travis', withBuildStep: true };
+    const withBuildEnv = Object.assign({}, baseEnv, { withBuildStep: true });
 
     describe('and npm is the detected npm client', () => {
-      const env = Object.assign({}, baseEnv, { npmClient: 'npm' });
+      const env = Object.assign({}, withBuildEnv, { npmClient: 'npm' });
 
       it('returns a configuration with the build step', () => {
         const config = buildReleaseConfig(env);
@@ -17,7 +18,7 @@ describe('buildReleaseConfig', () => {
     });
 
     describe('and yarn is the detected npm client', () => {
-      const env = Object.assign({}, baseEnv, { npmClient: 'yarn' });
+      const env = Object.assign({}, withBuildEnv, { npmClient: 'yarn' });
 
       it('returns a configuration with the build step', () => {
         const config = buildReleaseConfig(env);
@@ -30,16 +31,28 @@ describe('buildReleaseConfig', () => {
   });
 
   describe('when build script is not defined in package.json', () => {
-    const env = {
+    const withoutBuildEnv = Object.assign({}, baseEnv, {
       npmClient: 'yarn',
-      testRunner: 'travis',
       withBuildStep: false
-    };
+    });
 
     it('returns a configuration without the build step', () => {
-      const config = buildReleaseConfig(env);
+      const config = buildReleaseConfig(withoutBuildEnv);
 
       expect(config.build).toBeUndefined();
+    });
+  });
+
+  describe('when the configured branch is other than master', () => {
+    const customBranchEnv = Object.assign({}, baseEnv, {
+      npmClient: 'yarn',
+      branch: 'my-branch'
+    });
+
+    it('returns a configuration with a check ou step to the custom branch', () => {
+      const config = buildReleaseConfig(customBranchEnv);
+
+      expect(config.prepare[1]).toBe('git checkout my-branch');
     });
   });
 });
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,7 @@ const path = require('path');
 const VERSIONS = ['major', 'minor', 'patch'];
 const DEFAULT_TEST_RUNNER = 'travis';
 const VALID_TEST_RUNNERS = [DEFAULT_TEST_RUNNER, 'ci'];
+const DEFAULT_BRANCH = 'master';
 const DEFAULT_CONFIG_PATH = '.release.yml';
 const PACKAGE_JSON_PATH = path.resolve(process.env.PWD, 'package.json');
 const LERNA_JSON_PATH = path.resolve(process.env.PWD, 'lerna.json');
@@ -11,6 +12,7 @@ module.exports = {
   VERSIONS,
   DEFAULT_TEST_RUNNER,
   VALID_TEST_RUNNERS,
+  DEFAULT_BRANCH,
   DEFAULT_CONFIG_PATH,
   PACKAGE_JSON_PATH,
   LERNA_JSON_PATH
